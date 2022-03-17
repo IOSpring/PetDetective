4a5816071defd2c56e1c30e59b84b382b342c189
wonjongseo <visionwill@naver.com>
2022-03-17 16:34:36 +0900

    setup

---
+++ src/main/java/com/iospring/pets/petsfinder/PetsFinderApplication.java
@@ -0,0 +1,13 @@
+package com.iospring.pets.petsfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iospring/pets/petsfinder/PetsFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iospring.pets.petsfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
