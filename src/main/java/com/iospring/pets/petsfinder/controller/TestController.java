b6ff014c1bfb719de4f9fd935141dc5b6192a2b1
wonjongseo <visionwill@naver.com>
2022-03-18 12:14:36 +0900

    save file in local database

--- src/main/java/com/iospring/pets/petsfinder/controller/TestController.java
+++ src/main/java/com/iospring/pets/petsfinder/controller/TestController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @RestController
 public class TestController {
 
@@ -13,11 +16,30 @@ public class TestController {
         return "Hello~~";
     }
 
+    private final String FILE_PATH = "/Users/wonjongseo/aStudy/project/iospring/petsFinder/src/main/resources/static/img";
 
     @PostMapping("/images")
+    public String getImage(MultipartFile file) {
+        if (file == null) {
+            return "파일 안왔어용";
+        }
+
+        return file.getOriginalFilename();
+    }
+
+    @PostMapping("/images2")
     public String getImages(MultipartFile file) {
         if (file == null) {
             return "파일 안왔어용";
+        }
+        String uploadFileName = file.getOriginalFilename();
+        File saveFile = new File(FILE_PATH, uploadFileName);
+        try{
+            file.transferTo(saveFile);
+            return saveFile.getAbsolutePath();
+        } catch (IOException e) {
+            System.out.println("e.getMessage() = " + e.getMessage());
+            e.printStackTrace();
         }
         return file.getOriginalFilename();
     }
