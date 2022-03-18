bf70acbdf4b250b7d4949b5ce1816f5bdbca4a45
wonjongseo <visionwill@naver.com>
2022-03-18 12:09:12 +0900

    test upload file

--- src/main/java/com/iospring/pets/petsfinder/controller/TestController.java
+++ src/main/java/com/iospring/pets/petsfinder/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.iospring.pets.petsfinder.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class TestController {
@@ -10,4 +12,15 @@ public class TestController {
     public String test() {
         return "Hello~~";
     }
+
+
+    @PostMapping("/images")
+    public String getImages(MultipartFile file) {
+        if (file == null) {
+            return "파일 안왔어용";
+        }
+        return file.getOriginalFilename();
+    }
 }
+
+
