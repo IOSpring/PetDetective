72a643028b3063ca9fe389cc6883fad56ab05022
kbm6609 <kbm6609@naver.com>
2022-05-20 16:34:56 +0900

    update user location

--- src/main/java/com/iospring/pets/petsfinder/user/repositoru/LoginRepository.java
+++ src/main/java/com/iospring/pets/petsfinder/user/repositoru/LoginRepository.java
@@ -60,6 +60,7 @@ public class LoginRepository {
                 "select u from User u where u.phoneNumber = :phoneNumber",User.class
         ).setParameter("phoneNumber",userLocationDtoto.getPhoneNumber()).getSingleResult();
         if(u != null) {
+            em.persist(u);
             u.setLatitude(userLocationDtoto.getLatitude());
             u.setLongitude(userLocationDtoto.getLongitude());
             u.setLoadAddress(userLocationDtoto.getLoadAddress());
